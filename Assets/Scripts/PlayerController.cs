a90ca0159c754455c16b3f4a57f5c3c1166c626d
Ryty <225980@student.pwr.edu.pl>
2017-08-01 13:38:49 +0200

    Joystick, basic movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public VirtualJoystick controlJoystick;
+
+    private Vector2 moveDir;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        moveDir = Vector2.zero;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (controlJoystick.inputDirection != Vector2.zero)
+        {
+            moveDir = controlJoystick.inputDirection * moveSpeed;
+
+            float rot = Mathf.Rad2Deg * Mathf.Atan2(controlJoystick.inputDirection.y, controlJoystick.inputDirection.x) - 90f;
+
+            transform.rotation = Quaternion.AngleAxis(rot, Vector3.forward);
+
+            Debug.Log(rot);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(moveDir);   
+    }
+}
---
+++ Assets/Scripts/VirtualJoystick.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class VirtualJoystick : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
+{
+    public Vector2 inputDirection { get; set; }
+
+    private Image backgroundImage;
+    private Image joystickImage;
+
+    private void Start()
+    {
+        inputDirection = Vector2.zero;
+
+        backgroundImage = GetComponent<Image>();
+        joystickImage = this.transform.GetChild(0).GetComponentInChildren<Image>(); //Bierzemy to z pierwszego dziecka
+    }
+
+
+    public virtual void OnDrag(PointerEventData data)
+    {
+        Vector2 pos = Vector2.zero;
+        if(RectTransformUtility.ScreenPointToLocalPointInRectangle(backgroundImage.rectTransform, data.position, data.enterEventCamera, out pos)) //jeśli trafiliśmy w środek backgroundImage to możemy iść dalej ze skryptem
+        {
+            //Wyciągamy proporcje z pozycji
+            pos.x = pos.x / backgroundImage.rectTransform.sizeDelta.x;
+            pos.y = pos.y / backgroundImage.rectTransform.sizeDelta.y;
+
+            //Przepisujemy info do inputa 
+            inputDirection = new Vector2(pos.x * 2f, pos.y * 2f); //razy 2, ponieważ wtedy można dojść do 1.0 bez wychodzenia za obszar
+            //Jeśli trzeba to normalizujemy input
+            inputDirection = (inputDirection.magnitude > 1) ? inputDirection.normalized : inputDirection;
+
+            //Przesuwamy obrazek joysticka
+            joystickImage.rectTransform.anchoredPosition = new Vector2(inputDirection.x * backgroundImage.rectTransform.sizeDelta.x / 3, inputDirection.y * backgroundImage.rectTransform.sizeDelta.y / 3);
+        }
+    }
+
+    public virtual void OnPointerDown(PointerEventData data)
+    {
+        //Wzywamy OnDrag żeby przesuwać to joystick zwykłym przytrzymaniem
+        OnDrag(data);
+    }
+
+    public virtual void OnPointerUp(PointerEventData data)
+    {
+        inputDirection = Vector2.zero;
+        joystickImage.rectTransform.anchoredPosition = Vector2.zero;
+    }
+}
