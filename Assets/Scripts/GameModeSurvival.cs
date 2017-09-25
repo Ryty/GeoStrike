3eda12e6e1136364400dd460a82e7c5a4f51274b
Ryty <225980@student.pwr.edu.pl>
2017-09-25 16:48:40 +0200

    Basic enemy spawning and gameplay loop

--- Assets/Scripts/GameModeSurvival.cs
+++ Assets/Scripts/GameModeSurvival.cs
@@ -15,22 +15,25 @@ public class GameModeSurvival : GameMode
     public ESurvivalState gameState;
     public int intermissionTime;
     public int waveCount = 1;
+    public int enemyCount = 0;
     [HideInInspector]
     public int playerScore = 0, comboMeter = 1;
 
     private UIManager ui;
+    private SpawnerSurvival[] spawners;
 
     public override IEnumerator StartGame()
     {
         ui = FindObjectOfType<UIManager>();
+        spawners = FindObjectsOfType<SpawnerSurvival>();
 
         yield return StartCoroutine(HandleIntermission(10));
 
-     //   while(gameState != ESurvivalState.Over)
-     //   {
+        while(gameState != ESurvivalState.Over)
+        {
             yield return StartCoroutine(HandleWave());
-    //        yield return StartCoroutine(HandleIntermission(intermissionTime));
-    //    }
+            yield return StartCoroutine(HandleIntermission(intermissionTime));
+        }
 
         Debug.Log("Completed!");
 
@@ -65,6 +68,28 @@ public class GameModeSurvival : GameMode
     {
         gameState = ESurvivalState.Wave;
         ui.scoreText.enabled = true;
+
+        StartCoroutine(SpawnEnemies());
+
+        Debug.Log("Spawning enemies finished, moving on with wave");
+        while (enemyCount > 0)
+        {
+            yield return null;
+        }
+
+        ui.scoreText.enabled = false;
+        yield break;
+    }
+
+    private IEnumerator SpawnEnemies()
+    {
+        foreach(SpawnerSurvival spawner in spawners)
+        {
+            spawner.Spawn(2);
+        }
+
+        Debug.Log("Spawned " + enemyCount + " enemies.");
+
         yield break;
     }
 }
---
+++ Assets/Scripts/SpawnerSurvival.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerSurvival : MonoBehaviour
+{
+    public GameObject[] enemyPrefabs;
+
+    private BoxCollider2D boxComp;
+    private GameModeSurvival gameMode;
+
+    private void Start()
+    {
+        boxComp = GetComponent<BoxCollider2D>();
+        gameMode = FindObjectOfType<GameModeSurvival>();
+    }
+
+    public void Spawn(int amount)
+    {
+        for(int i = 0; i < amount; i++)
+        {
+            Vector3 spawnPos = new Vector3(MathLibrary.RandomPointInBounds(boxComp.bounds).x, MathLibrary.RandomPointInBounds(boxComp.bounds).y, 1);
+
+            //GameObject go = 
+            Instantiate(enemyPrefabs[Random.Range(0, enemyPrefabs.Length)], spawnPos, new Quaternion());
+
+            gameMode.enemyCount++;
+        }
+    }
+}
